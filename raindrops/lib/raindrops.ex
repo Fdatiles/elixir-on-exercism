709fbaa3600c21a133c0e5152899d6d4f143594b
Fdatiles <68522452+Fdatiles@users.noreply.github.com>
2023-02-28 18:07:08 -0300

    Add raindrops solution

---
+++ raindrops/lib/raindrops.ex
@@ -0,0 +1,36 @@
+defmodule Raindrops do
+  @divisor_labels %{
+    3 => "Pling",
+    5 => "Plang",
+    7 => "Plong"
+  }
+
+  @doc """
+  Returns a string based on raindrop factors.
+
+  - If the number contains 3 as a prime factor, output 'Pling'.
+  - If the number contains 5 as a prime factor, output 'Plang'.
+  - If the number contains 7 as a prime factor, output 'Plong'.
+  - If the number does not contain 3, 5, or 7 as a prime factor,
+    just pass the number's digits straight through.
+  """
+  @spec convert(pos_integer) :: String.t()
+  def convert(number) do
+    out =
+      @divisor_labels
+      |> Enum.sort_by(fn {k, _v} -> k end)
+      |> Enum.map_join(fn {divisor, label} ->
+        if rem(number, divisor) == 0 do
+          label
+        else
+          ""
+        end
+      end)
+
+    if out == "" do
+      Integer.to_string(number)
+    else
+      out
+    end
+  end
+end
